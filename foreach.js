9bbc741cbff5254912a7e31912162c55accd4131
walk-ingegg <walk-ingegg@omuken.me>
2024-02-04 16:32:48 +0900

    add palindrome function

---
+++ foreach.js
@@ -0,0 +1,5 @@
+let a = ["and", "bad", "cat", 42];
+a.forEach((str) => console.log(str));
+
+let soliloquy = "To be, or not to be, that is the question:";
+Array.from(soliloquy).forEach((str) => console.log(str));
