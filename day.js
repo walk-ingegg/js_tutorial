478bc81558bf3fa0b748ec3acc8bfc607b23a389
walk-ingegg <walk-ingegg@omuken.me>
2024-02-04 16:04:53 +0900

    add comment

--- day.js
+++ day.js
@@ -1,3 +1,4 @@
+// 指定の日付に対応する曜日名を返す
 function dayName(date) {
   const daysOfTheWeek = [
     "日曜日",
@@ -11,6 +12,7 @@ function dayName(date) {
   return daysOfTheWeek[date.getDay()];
 }
 
+// 指定の日付の曜日を含む挨拶文をアラートで返す
 function greeting(date) {
   return `Hello, world! 素敵な${dayName(date)}になりますように。`;
 }
